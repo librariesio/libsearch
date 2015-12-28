22e13af5126b3d8d81d0320fef03ffc823969783
Andrew Nesbitt <andrewnez@gmail.com>
2015-12-28 22:45:54 +0000

    Initial commit

---
+++ libsearch.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+require 'commander/import'
+require 'typhoeus'
+require 'ansi/core'
+
+program :version, '0.0.1'
+program :description, 'command line search of Libraries.io'
+
+default_command :search
+
+command :search do |c|
+  c.syntax = 'libsearch query'
+  c.summary = ''
+  c.description = ''
+  c.example 'description', 'command example'
+  c.option '--platform PLATFORM', String, 'search a particular package manager'
+  c.option '--language LANGUAGE', String, 'search for libraries written in a language'
+  c.option '--debug', 'output helpful debug info'
+  c.action do |args, options|
+    puts
+    search_url = "https://libraries.io/api/search?q=#{args.first}&platforms=#{options.platform}&languages=#{options.language}"
+    if options.debug
+      puts "debug: #{search_url}"
+      puts
+    end
+
+    results = JSON.parse Typhoeus.get(search_url).response_body
+    results[0..5].each do |result|
+      puts " #{result['name'].ansi(:bold)} - #{result['platform'].ansi(:white)} - #{result['language'].ansi(:yellow)}"
+      description = result['description']
+      description = "#{result['description'][0..60]}..." if result['description'].length > 60
+      puts " #{description}"
+      puts " #{result['package_manager_url'].ansi(:underline)}"
+      puts
+    end
+  end
+end
